c9965d493f38e970aefbc2527b97e216aeac51b1
phantomhieve <khetanatulz@gmail.com>
2018-06-22 14:29:50 +0530

    8th class

--- class-7/image.py
+++ class-7/image.py
@@ -13,12 +13,25 @@ print img0.shape
 # parameters
     # windowName
     # data
+
+#to draw a line in the image
+#   image data,start point , end point, color, line width
+cv2.line(img1,(0,0),(150,100),(0,0,255),4)
+
+# cv2.rectangle() -> two ppint of hp , color, line width
+# cv2.circle() -> center,radius,color,line width
+
+# Putting text on image 
+# font = cv2.FONT_HERSEY_SIMPLEX 
+# cv2.putTEXT(img1,"NEKO",(10,10),font,3,(100,23,200))
+# data ,text ,starting point, font type, size, color
+
 cv2.imshow("windowname",img1)
 cv2.imshow("Gray",img0)
 cv2.imshow("Trans",img_1)
 
 # save black and white (GRAY) image
-cv2.imwrite()
+cv2.imwrite("new_name.jpeg",img1)
 
 #to hold upper window
 cv2.waitKey(0)
